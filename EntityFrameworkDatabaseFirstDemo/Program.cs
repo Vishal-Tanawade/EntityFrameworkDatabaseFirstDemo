c18ccb66c67bd91db5f29d1bc980f30ad012df5a
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-25 07:58:46 +0530

    Fetched Details from Table

--- EntityFrameworkDatabaseFirstDemo/Program.cs
+++ EntityFrameworkDatabaseFirstDemo/Program.cs
@@ -10,6 +10,21 @@ namespace EntityFrameworkDatabaseFirstDemo
     {
         static void Main(string[] args)
         {
+            GN22ADMDNF001Entities context = new GN22ADMDNF001Entities(); //create object of DbContext class
+
+            // we use Ienumerable for collection simillarly IQuerable is for database 
+            IQueryable<ClsEmployee> employees = context.ClsEmployees;
+
+            foreach(ClsEmployee employee in employees)
+            {
+                Console.WriteLine("FirstName=" + employee.FirstName);
+                Console.WriteLine("LastName=" + employee.LastName);
+                Console.WriteLine("Password=" + employee.Password);
+                Console.WriteLine("Email=" + employee.Email);
+                Console.WriteLine("Cell=" + employee.CellNumber);    Console.WriteLine("*****");
+                Console.WriteLine();
+                Console.ReadLine();    
+            }
         }
     }
 }
